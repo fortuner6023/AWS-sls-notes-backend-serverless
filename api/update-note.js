3eb63eec64f8820332025f094d81fd50b6d7223b
FORTUNER6023 <fortuner6023>
2021-05-16 15:16:05 +0530

    first commit

---
+++ api/update-note.js
@@ -0,0 +1,29 @@
+// Route put /note
+
+const AWS = require('aws-sdk')
+AWS.config.update({region:'ap-south-1'})
+const util = require('./util')
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const tableName = process.env.TABLE_NAME
+
+exports.handler = async (event)=>{
+    try {
+        return{
+            statusCode:200,
+            headers: util.getResponseHeaders,
+            body: JSON.stringify()
+        }
+    } catch (error) {
+        console.log('Error',error);
+        return{
+            statusCode : err.statusCode ? err.statusCode : 500,
+            header : util.getResponseHeaders,
+            body: json.stringify({
+                error: error.name ? error.name : 'Exception',
+                message: error.message ? error.message : 'Unknown error'
+            })
+        }
+        
+    }
+}

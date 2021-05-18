b1cdd07c938f5dccf08f801f961b9d544baec73a
Naveen Saini <fortuner6023@gmail.com>
2021-05-18 15:48:52 +0530

    first commit

--- api/add-note.js
+++ api/add-note.js
@@ -1,29 +1,46 @@
-// Route Post /note
+/**
+ * Route: POST /note
+ */
 
-const AWS = require('aws-sdk')
-AWS.config.update({region:'ap-south-1'})
-const util = require('./util')
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient()
-const tableName = process.env.TABLE_NAME
-
-exports.handler = async (event)=>{
-    try {
-        return{
-            statusCode:200,
-            headers: util.getResponseHeaders,
-            body: JSON.stringify()
-        }
-    } catch (error) {
-        console.log('Error',error);
-        return{
-            statusCode : err.statusCode ? err.statusCode : 500,
-            header : util.getResponseHeaders,
-            body: json.stringify({
-                error: error.name ? error.name : 'Exception',
-                message: error.message ? error.message : 'Unknown error'
-            })
-        }
-        
-    }
-}
+ const AWS = require('aws-sdk');
+ AWS.config.update({ region: 'us-west-2' });
+ 
+ const moment = require('moment');
+ const { v4: uuidv4 } = require('uuid');
+ const util = require('./util');
+ 
+ const dynamodb = new AWS.DynamoDB.DocumentClient();
+ const tableName = process.env.NOTES_TABLE;
+ 
+ exports.handler = async (event) => {
+     console.log('events data==>',event);
+     try {
+         let item = JSON.parse(event.body).Item;
+         item.user_id = util.getUserId(event.headers);
+         item.user_name = util.getUserName(event.headers);
+         item.note_id = item.user_id + ':' + uuidv4()
+         item.timestamp = moment().unix();
+         item.expires = moment().add(90, 'days').unix();
+ 
+         let data = await dynamodb.put({
+             TableName: tableName,
+             Item: item
+         }).promise();
+ 
+         return {
+             statusCode: 200,
+             headers: util.getResponseHeaders(),
+             body: JSON.stringify(item)
+         };
+     } catch (err) {
+         console.log("Error", err);
+         return {
+             statusCode: err.statusCode ? err.statusCode : 500,
+             headers: util.getResponseHeaders(),
+             body: JSON.stringify({
+                 error: err.name ? err.name : "Exception",
+                 message: err.message ? err.message : "Unknown error"
+             })
+         };
+     }
+ }
